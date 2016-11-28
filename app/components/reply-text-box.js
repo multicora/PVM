0f0ccb1ff99c87ec118df269d08b53b6e4cce4f7
FeditskiyRoman <feditskiy@gmail.com>
2016-11-28 19:19:56 +0200

    BIZ-22 added button disabled

--- app/components/reply-text-box.js
+++ app/components/reply-text-box.js
@@ -2,7 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 	isShownTextArea: false,
+	disabledButton: true,
 	click() {
+    this.setProperties({disabledButton: 'false'});
     this.setProperties({isShownTextArea: 'true'});
   }
 });
