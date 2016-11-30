23e17bf59d4ae8e68c3717a1cbec9fc4d74b3a29
FeditskiyRoman <feditskiy@gmail.com>
2016-11-30 17:52:52 +0200

    BIZ-38 added button logic

--- app/components/video-record.js
+++ app/components/video-record.js
@@ -1,37 +1,37 @@
 import Ember from 'ember';
-import Record from 'npm:videojs-record';
+// import Record from 'npm:videojs-record';
 export default Ember.Component.extend({
 	isShown: true,
   actions: {
     hidePopup() {
       this.toggleProperty('isShown');
     },
-    uploadVideo(event) {
+    uploadVideo() {
       
     }
   },
-	player: {
-    controls: true,
-    loop: false,
-    // dimensions of video.js player
-    width: 100,
-    height: 200,
-    plugins: {
-      record: {
-        maxLength: 5,
-        debug: true,
-        audio: false,
-        video: {
-          // video constraints: set resolution of camera
-          mandatory: {
-            minWidth: 100,
-            minHeight: 200,
-          },
-        },
-        // dimensions of captured video frames
-        frameWidth: 100,
-        frameHeight: 200
-	    }
-	  }
-	 }
+	// player: {
+ //    controls: true,
+ //    loop: false,
+ //    // dimensions of video.js player
+ //    width: 100,
+ //    height: 200,
+ //    plugins: {
+ //      record: {
+ //        maxLength: 5,
+ //        debug: true,
+ //        audio: false,
+ //        video: {
+ //          // video constraints: set resolution of camera
+ //          mandatory: {
+ //            minWidth: 100,
+ //            minHeight: 200,
+ //          },
+ //        },
+ //        // dimensions of captured video frames
+ //        frameWidth: 100,
+ //        frameHeight: 200
+	//     }
+	//   }
+	//  }
 });
