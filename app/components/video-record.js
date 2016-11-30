34685fe5c135afdf0921bb8789342d395665369f
FeditskiyRoman <feditskiy@gmail.com>
2016-11-30 17:46:24 +0200

    BIZ-27 added fonts

--- app/components/video-record.js
+++ app/components/video-record.js
@@ -1,29 +1,37 @@
 import Ember from 'ember';
-// import Record from 'npm:videojs-record';
+import Record from 'npm:videojs-record';
 export default Ember.Component.extend({
 	isShown: true,
-	// player: {
- //    controls: true,
- //    loop: false,
- //    // dimensions of video.js player
- //    width: 100,
- //    height: 200,
- //    plugins: {
- //      record: {
- //        maxLength: 5,
- //        debug: true,
- //        audio: false,
- //        video: {
- //          // video constraints: set resolution of camera
- //          mandatory: {
- //            minWidth: 100,
- //            minHeight: 200,
- //          },
- //        },
- //        // dimensions of captured video frames
- //        frameWidth: 100,
- //        frameHeight: 200
-	//     }
-	//   }
-	//  }
+  actions: {
+    hidePopup() {
+      this.toggleProperty('isShown');
+    },
+    uploadVideo(event) {
+      
+    }
+  },
+	player: {
+    controls: true,
+    loop: false,
+    // dimensions of video.js player
+    width: 100,
+    height: 200,
+    plugins: {
+      record: {
+        maxLength: 5,
+        debug: true,
+        audio: false,
+        video: {
+          // video constraints: set resolution of camera
+          mandatory: {
+            minWidth: 100,
+            minHeight: 200,
+          },
+        },
+        // dimensions of captured video frames
+        frameWidth: 100,
+        frameHeight: 200
+	    }
+	  }
+	 }
 });
