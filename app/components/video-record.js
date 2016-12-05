997a156c1334b0b599594e46f54467f0446c98e9
Dmytro Borchuk <Dmytro.Borchuk@symphony-solutions.eu>
2016-12-05 23:09:10 +0200

    Added upload recorded video

--- app/components/video-record.js
+++ app/components/video-record.js
@@ -1,13 +1,33 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  uploader: Ember.inject.service(),
   isShown: true,
   actions: {
     hidePopup() {
       this.toggleProperty('isShown');
     },
     uploadVideo() {
-    }
+    },
+    ready(player, component) {
+      let uploader = this.get('uploader');
+      player.on('finishRecord', function() {
+        let audioFile = player.recordedData;
+
+        uploader.video({
+          file: audioFile.video
+        }).then(
+          () => {
+            console.log('finish');
+          },
+          (error) => {
+            console.log('File uploading failure:');
+            console.log(error);
+          }
+        );
+
+      });
+    },
   },
   setup: {
     controls: true,
