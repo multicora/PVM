aba268639f549d753b6b9e2a6afec8b22a5bf1ba
FeditskiyRoman <feditskiy@gmail.com>
2016-12-06 14:59:01 +0200

    BIZ-38 added disabled button functionality

--- app/components/video-record.js
+++ app/components/video-record.js
@@ -1,19 +1,46 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  uploader: Ember.inject.service(),
   isShown: true,
   isUploading: false,
+  disabledButton: true,
+  audioFile: null,
   actions: {
     hidePopup() {
       this.toggleProperty('isShown');
     },
-    uploadVideo() {
-    }
+    saveVideo() {
+      let audioFile =this.get('audioFile');
+      let uploader = this.get('uploader');
+      uploader.video({
+        file: audioFile.video
+      }).then(
+        () => {
+          console.log('finish');
+        },
+        (error) => {
+          console.log('File uploading failure:');
+          console.log(error);
+        }
+      );
+    },
+    ready(player, component) {
+      let self = this;
+      player.on('finishRecord', function() {
+        self.set('disabledButton', false);
+        self.set('audioFile', player.recordedData);
+      });
+
+      player.on('startRecord', function() {
+        self.set('disabledButton', true);
+      })
+    },
   },
   setup: {
     controls: true,
-    width: 320,
-    height: 240,
+    width: 640,
+    height: 480,
     plugins: {
       record: {
         audio: true,
