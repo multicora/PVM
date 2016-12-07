38f6720ae3f55c5e227491940a21cce0d2749bbb
FeditskiyRoman <feditskiy@gmail.com>
2016-12-07 17:11:06 +0200

    BIZ-51 fixed collapse

--- app/components/watch-buttons.js
+++ app/components/watch-buttons.js
@@ -1,13 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  isHovered: false,
-  actions: {
-    hover() {
-      this.setProperties({isHovered: 'true'});
-    }
-  },
-  mouseLeave() {
-    this.toggleProperty('isHovered');
-  }
 });
