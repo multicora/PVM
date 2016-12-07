89dd11ffedacfba1ebb1c0c9ee1425c39314f4ae
Dmytro Borchuk <Dmytro.Borchuk@symphony-solutions.eu>
2016-12-07 18:10:23 +0200

    Fixed buttons

--- app/components/reply-text-box.js
+++ app/components/reply-text-box.js
@@ -1,9 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-	isShownTextArea: false,
-	disabledButton: true,
-	click() {
+  isShownTextArea: false,
+  disabledButton: true,
+  click() {
+    this.set('disabledButton', false);
+    this.set('isShownTextArea', true);
+  },
+  touchEnd() {
     this.set('disabledButton', false);
     this.set('isShownTextArea', true);
   }
