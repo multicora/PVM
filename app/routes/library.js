e503cb7709101c62e3763f000f1d0e57d2887e4f
FeditskiyRoman <feditskiy@gmail.com>
2016-12-12 13:00:55 +0200

    BIZ-32 fixed

--- app/routes/library.js
+++ app/routes/library.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
-    // return this.store.findAll('video');
-    return [];
+    return this.store.findAll('video');
+    // return [];
   }
 });
