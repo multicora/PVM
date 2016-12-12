41d005075b503156a27e97588898a0ff80e0fe86
FeditskiyRoman <feditskiy@gmail.com>
2016-12-12 13:04:32 +0200

    BIZ-32 fixed

--- app/components/library-container.js
+++ app/components/library-container.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  isShownRecord: false,
-  isShownUpload: false
+  isShownRecordPopup: false,
+  isShownUploadPopup: false
 });
