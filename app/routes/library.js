49d82f07caa1f49433e90e259192838d34c325dc
Dmytro Borchuk <Dmytro.Borchuk@symphony-solutions.eu>
2016-12-15 16:07:00 +0200

    Added 401 handling in library

--- app/routes/library.js
+++ app/routes/library.js
@@ -5,6 +5,13 @@ export default Ember.Route.extend({
     return this.store.findAll('video');
   },
   actions: {
+    error: function(err) {
+      if (err.errors[0].status === '401') {
+        this.transitionTo('auth');
+      } else {
+        console.log(err.message);
+      }
+    },
     redirectToAuth(param) {
       console.log(123123123123123123123);
       console.log(param);
