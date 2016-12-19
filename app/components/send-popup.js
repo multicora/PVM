b345ff0fcd6dbba0e250ad2f31f6860793e29fd4
FeditskiyRoman <feditskiy@gmail.com>
2016-12-19 15:52:36 +0200

    BIZ-89 working version

--- app/components/send-popup.js
+++ app/components/send-popup.js
@@ -2,15 +2,17 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   store: Ember.inject.service(),
-  // email: 'bizkonect.project@gmail.com',
+  email: 'bizkonect.project@gmail.com',
   actions: {
     sendVideo() {
-      console.log(this.get('videoId'));
-      // let email = this.getProperties('email');
-      // let video = this.get('videoId');
-      // let author = '1';
-      // let record = this.get('store').createRecord('conversation', {'email': email, 'video': video, 'author': author});
-      // record.save();
+      let email = this.get('email');
+      // let email = this.getProperties('email').email;
+      let video = this.get('videoId');
+      let record = this.get('store').createRecord('conversation', {'email': email, 'video': video});
+      record.save();
+    },
+    hidePopup() {
+      this.set('isShownSendPopup', false);
     }
   }
 });
