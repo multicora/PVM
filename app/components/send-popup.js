563c39b72996b654bc5e26550589d718596abb4d
FeditskiyRoman <feditskiy@gmail.com>
2016-12-21 10:50:33 +0200

    little fix

--- app/components/send-popup.js
+++ app/components/send-popup.js
@@ -2,12 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   store: Ember.inject.service(),
-  email: 'bizkonect.project@gmail.com',
   actions: {
     sendVideo() {
-      let email = this.get('email');
+      let email = this.getProperties('email');
       let video = this.get('videoId');
-      let record = this.get('store').createRecord('conversation', {'email': email, 'video': video});
+      let record = this.get('store').createRecord('conversation', {'email': email.email, 'video': video});
       record.save();
     },
     hidePopup() {
