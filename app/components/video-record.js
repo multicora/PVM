b81d23e39e50a7c4ae3cb610aeb57c8f1e03d86a
FeditskiyRoman <feditskiy@gmail.com>
2016-12-27 15:30:52 +0200

    BIZ-68 added video name input

--- app/components/video-record.js
+++ app/components/video-record.js
@@ -12,13 +12,14 @@ export default Ember.Component.extend({
     },
     saveVideo() {
       let audioFile =this.get('audioFile');
+      audioFile.video.name = this.getProperties('videoName').videoName;
       let uploader = this.get('uploader');
       let uplaodStart = this.set('isUploading', true);
       let uplaodEnd = this.set('isUploading', false);
       uploader.video({
         file: audioFile.video,
-        uploadStart: uploadStart,
-        uploadEnd: uplaodEnd
+        // uploadStart: uploadStart,
+        // uploadEnd: uplaodEnd
       }).then(
         () => {
           console.log('finish');
