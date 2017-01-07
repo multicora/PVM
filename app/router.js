f56ca490617020fc5f17f5cc15ac5e648fd44d3e
Dmytro Borchuk <frunk.lern@gmail.com>
2017-01-07 22:48:29 +0200

    BIZ-98 Google analitics

--- app/router.js
+++ app/router.js
@@ -1,8 +1,8 @@
 import Ember from 'ember';
 import config from './config/environment';
-import googlePageview from './mixins/google-pageview';
+// import googlePageview from './mixins/google-pageview';
 
-const Router = Ember.Router.extend(googlePageview, {
+const Router = Ember.Router.extend({
   location: config.locationType,
   rootURL: config.rootURL
 });
@@ -11,6 +11,15 @@ Router.map(function() {
   this.route('conversation', {path: 'conversation/:conversation_id'});
   this.route('library');
   this.route('auth');
+});
+
+Router.reopen({
+  notifyGoogleAnalytics: function() {
+    return ga('send', 'pageview', {
+        'page': this.get('url'),
+        'title': this.get('url')
+      });
+  }.on('didTransition')
 });
 
 export default Router;
--- config/environment.js
+++ config/environment.js
@@ -54,11 +54,5 @@ module.exports = function(environment) {
     ENV.APP.rootElement = '#ember-testing';
   }
 
-  if (environment === 'production') {
-    ENV.googleAnalytics = {
-      webPropertyId: 'UA-89625506-1'
-    };
-  }
-
   return ENV;
 };
