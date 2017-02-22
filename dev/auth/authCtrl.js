83c4daff0e891be9cd1124a1ac6011ec48ebbfb4
FeditskiyRoman <feditskiy@gmail.com>
2017-02-22 12:39:06 +0200

    BIZ-161 fixed

--- dev/auth/authCtrl.js
+++ dev/auth/authCtrl.js
@@ -3,31 +3,34 @@
 
   app.controller('authCtrl', ctrl);
 
-  ctrl.$inject = ['$location', '$routeParams', 'authService', 'tokenService', '$scope'];
-  function ctrl($location, $routeParams, authService, tokenService, $scope) {
+  ctrl.$inject = [
+    '$location',
+    '$routeParams',
+    'authService',
+    'tokenService'
+  ];
+  function ctrl(
+    $location,
+    $routeParams,
+    authService,
+    tokenService
+  ) {
     var vm = this;
 
     vm.authenticate = function(login, password) {
       authService.login(login, password).then(function (res) {
-        if (res.data.error) {
-          vm.errorMessage = res.data.message;
-        } else {
-          tokenService.setToken(res.data.token);
-          $location.path('/');
-          // TODO: browser should ask to store password
-        }
+        tokenService.setToken(res.data.token);
+        $location.path('/');
+      }, function(err) {
+        vm.errorMessage = err;
       });
     }
 
     vm.register = function(email, password, confirmPassword) {
       authService.register(email, password, confirmPassword).then(function(res) {
-        if (res.data.error) {
-          vm.errorRegister = res.data.message;
-        } else {
-          $location.path('/login');
-        }
+        $location.path('/login');
       }, function(err) {
-        vm.errorRegister = res.data.message;
+        vm.errorRegister = err;
       });
     }
 
