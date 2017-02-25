53fda92838cb68bfadb41e6c0b0edbfec806284c
Dmytro Borchuk <frunk.lern@gmail.com>
2017-02-25 12:29:23 +0200

    Removed files

--- app/models/conversation.js
+++
@@ -1,7 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.Model.extend({
-  email: DS.attr('string'),
-  video: DS.attr('string'),
-  author: DS.attr('string')
-});
--- app/routes/auth.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Route.extend({
-  actions: {
-    redirect(uri) {
-      this.transitionTo(uri);
-    }
-  }
-});
