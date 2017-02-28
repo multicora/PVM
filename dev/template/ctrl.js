b9256d33b8301fbb9522d00ccbc1d35d94d576f1
FeditskiyRoman <feditskiy@gmail.com>
2017-02-28 15:34:43 +0200

    BIZ-226 added markup

--- dev/template/ctrl.js
+++ dev/template/ctrl.js
@@ -7,22 +7,5 @@
   function ctrl($routeParams, conversationsService) {
     var vm = this;
 
-    vm.showChatBox = false;
-    vm.conversation = null;
-    vm.media = null;
-
-    conversationsService.get($routeParams.id).then(function (res) {
-      vm.conversation = res.data;
-      vm.media = {
-        sources: [{
-          src: vm.conversation.url,
-          type: 'video/mp4'
-        }]
-      };
-    });
-
-    vm.chatBoxClick = function () {
-      vm.showChatBox = true;
-    };
   }
 })(angular);
