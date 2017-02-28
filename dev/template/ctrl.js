c44be16e70e428e5ed02791e393ec744fa01cb5d
FeditskiyRoman <feditskiy@gmail.com>
2017-02-28 11:26:24 +0200

    BIZ-153 added template markup

---
+++ dev/template/ctrl.js
@@ -0,0 +1,28 @@
+(function (angular) {
+  var app = angular.module('app');
+
+  app.controller('templateCtrl', ctrl);
+
+  ctrl.$inject = ['$routeParams', 'conversationsService'];
+  function ctrl($routeParams, conversationsService) {
+    var vm = this;
+
+    vm.showChatBox = false;
+    vm.conversation = null;
+    vm.media = null;
+
+    conversationsService.get($routeParams.id).then(function (res) {
+      vm.conversation = res.data;
+      vm.media = {
+        sources: [{
+          src: vm.conversation.url,
+          type: 'video/mp4'
+        }]
+      };
+    });
+
+    vm.chatBoxClick = function () {
+      vm.showChatBox = true;
+    };
+  }
+})(angular);
