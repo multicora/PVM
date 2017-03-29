bca4ef9545a28faa14fb7636513488f89137445d
FeditskiyRoman <feditskiy@gmail.com>
2017-03-29 16:51:29 +0300

    BIZ-241 added template for dashboard

---
+++ dev/dashboard/ctrl.js
@@ -0,0 +1,13 @@
+(function (angular) {
+  var app = angular.module('app');
+
+  app.controller('dashboardCtrl', ctrl);
+
+  ctrl.$inject = [
+  ];
+  function ctrl(
+  ) {
+    var vm = this;
+    console.log(111111);
+  }
+})(angular);
