42d347053de91cc786b4a32506a81d6efbccd3d8
dmitrij-borchuk <frunk.lern@gmail.com>
2017-05-29 15:34:00 +0300

    Fixed tests

--- dev/template/ctrl.spec.js
+++ dev/template/ctrl.spec.js
@@ -4,22 +4,11 @@ describe('templateCtrl', function() {
   var $q;
   var $rootScope;
   var $routeParams;
-  var conversationsService = {
-    create: jasmine.createSpy('create'),
-    createPublic: jasmine.createSpy('createPublic'),
-    getVideo: jasmine.createSpy('getVideo'),
-  };
-  var libraryService = {
-    getVideos: jasmine.createSpy('getVideos'),
-  };
-  var filesService = {
-    getFiles: jasmine.createSpy('getFiles'),
-  };
-  var templateService = {
-    getTemplate: jasmine.createSpy('getTemplate'),
-    createTemplate: jasmine.createSpy('createTemplate'),
-    updateTemplate: jasmine.createSpy('updateTemplate'),
-  };
+  var conversationsService;
+  var libraryService;
+  var filesService;
+  var profileService;
+  var templateService;
   var uploadRecordPopupService = {
     showUploadPopup: jasmine.createSpy('showUploadPopup'),
     showRecordPopup: jasmine.createSpy('showRecordPopup'),
@@ -31,12 +20,77 @@ describe('templateCtrl', function() {
     // The injector unwraps the underscores (_) from around the parameter names when matching
     $controller = _$controller_;
     $rootScope = _$rootScope_;
-    $routeParams = _$routeParams_;
     $q = _$q_;
+
+    $routeParams = {};
+    templateService = {
+      getTemplate: jasmine.createSpy('getTemplate'),
+      createTemplate: jasmine.createSpy('createTemplate'),
+      updateTemplate: jasmine.createSpy('updateTemplate'),
+    };
+    libraryService = {
+      getVideos: jasmine.createSpy('getVideos').and.callFake(function () {
+        return $q.resolve({data:[]});
+      })
+    };
+    conversationsService = {
+      create: jasmine.createSpy('create'),
+      createPublic: jasmine.createSpy('createPublic'),
+      getVideo: jasmine.createSpy('getVideo').and.callFake(function () {
+        return $q.resolve({
+          data: {
+            attributes: {
+              url:''
+            }
+          }
+        });
+      }),
+    };
+    profileService = {
+      getProfile: jasmine.createSpy('getProfile').and.callFake(function () {
+        return $q.resolve({data:[]});
+      }),
+    };
+    filesService = {
+      getFiles: jasmine.createSpy('getFiles').and.callFake(function () {
+        return $q.resolve({
+          data: [{
+            name: 1
+          }, {
+            name: 3
+          }, {
+            name: 2
+          }]
+        });
+      }),
+    };
   }));
 
-  describe('getTemplate', function() {
-    it('should get template if "$routeParams.templateId" ', function() {
+  it('should be defined', function() {
+    var scope = $rootScope.$new();
+
+    templateService.getTemplate.and.callFake(function () {
+      return $q.resolve({
+        data: []
+      });
+    });
+
+    var ctrl = $controller('templateCtrl', {
+      $scope: scope,
+      templateService: templateService,
+      libraryService: libraryService,
+      filesService: filesService,
+      conversationsService: conversationsService,
+      profileService: profileService,
+    });
+
+    scope.$apply();
+
+    expect(ctrl).toBeDefined();
+  });
+
+  describe('getVideos', function() {
+    it('should set "vm.videoList"', function() {
       var scope = $rootScope.$new();
 
       templateService.getTemplate.and.callFake(function () {
@@ -59,40 +113,43 @@ describe('templateCtrl', function() {
       var ctrl = $controller('templateCtrl', {
         $scope: scope,
         templateService: templateService,
+        libraryService: libraryService,
         filesService: filesService,
-      });
-
-      scope.$apply();
-  //     expect(ctrl.messages).toBeTruthy();
-    });
-
-    it('should get video if "$routeParams.video" ', function() {
-      var scope = $rootScope.$new();
-
-      conversationsService.getVideo.and.callFake(function () {
-        return $q.resolve({
-          data: []
-        });
-      });
-
-      var ctrl = $controller('templateCtrl', {
-        $scope: scope,
         conversationsService: conversationsService,
+        profileService: profileService,
       });
 
       scope.$apply();
-  //     expect(ctrl.messages).toBeTruthy();
+      expect(ctrl.videoList).toBeDefined();
     });
+
+  //   it('should get video if "$routeParams.video" ', function() {
+  //     var scope = $rootScope.$new();
+
+  //     conversationsService.getVideo.and.callFake(function () {
+  //       return $q.resolve({
+  //         data: []
+  //       });
+  //     });
+
+  //     var ctrl = $controller('templateCtrl', {
+  //       $scope: scope,
+  //       conversationsService: conversationsService,
+  //     });
+
+  //     scope.$apply();
+  // //     expect(ctrl.messages).toBeTruthy();
+  //   });
   });
 
-  function getDependency() {
-    return {
-      uploadRecordPopupService: uploadRecordPopupService,
-      conversationsService: conversationsService,
-      uploadService: uploadService,
-      libraryService: libraryService,
-      templateService: templateService,
-      filesService: filesService,
-    };
-  }
+  // function getDependency() {
+  //   return {
+  //     uploadRecordPopupService: uploadRecordPopupService,
+  //     conversationsService: conversationsService,
+  //     uploadService: uploadService,
+  //     libraryService: libraryService,
+  //     templateService: templateService,
+  //     filesService: filesService,
+  //   };
+  // }
 });
