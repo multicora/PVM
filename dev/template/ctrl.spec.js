a346bdf91efe6618d246efbfeb61ea064c0bd29e
FeditskiyRoman <feditskiy@gmail.com>
2017-05-30 12:10:14 +0300

    BIZ-278 Added few tests

--- dev/template/ctrl.spec.js
+++ dev/template/ctrl.spec.js
@@ -4,23 +4,22 @@ describe('templateCtrl', function() {
   var $q;
   var $rootScope;
   var $routeParams;
+  var $location;
   var conversationsService;
   var libraryService;
   var filesService;
   var profileService;
   var templateService;
-  var uploadRecordPopupService = {
-    showUploadPopup: jasmine.createSpy('showUploadPopup'),
-    showRecordPopup: jasmine.createSpy('showRecordPopup'),
-  };
+  var uploadRecordPopupService;
   var uploadService = {};
 
   beforeEach(module('app'));
-  beforeEach(inject(function(_$controller_, _$rootScope_, _$q_){
+  beforeEach(inject(function(_$controller_, _$rootScope_, _$q_, _$location_){
     // The injector unwraps the underscores (_) from around the parameter names when matching
     $controller = _$controller_;
     $rootScope = _$rootScope_;
     $q = _$q_;
+    $location = _$location_;
 
     $routeParams = {};
     templateService = {
@@ -33,8 +32,14 @@ describe('templateCtrl', function() {
         return $q.resolve({data:[]});
       })
     };
+    uploadRecordPopupService = {
+      showUploadPopup: jasmine.createSpy('showUploadPopup'),
+      showRecordPopup: jasmine.createSpy('showRecordPopup'),
+    };
     conversationsService = {
-      create: jasmine.createSpy('create'),
+      create: jasmine.createSpy('create').and.callFake(function () {
+        return $q.resolve();
+      }),
       createPublic: jasmine.createSpy('createPublic'),
       getVideo: jasmine.createSpy('getVideo').and.callFake(function () {
         return $q.resolve({
@@ -122,34 +127,279 @@ describe('templateCtrl', function() {
       scope.$apply();
       expect(ctrl.videoList).toBeDefined();
     });
-
-  //   it('should get video if "$routeParams.video" ', function() {
-  //     var scope = $rootScope.$new();
-
-  //     conversationsService.getVideo.and.callFake(function () {
-  //       return $q.resolve({
-  //         data: []
-  //       });
-  //     });
-
-  //     var ctrl = $controller('templateCtrl', {
-  //       $scope: scope,
-  //       conversationsService: conversationsService,
-  //     });
-
-  //     scope.$apply();
-  // //     expect(ctrl.messages).toBeTruthy();
-  //   });
   });
 
-  // function getDependency() {
-  //   return {
-  //     uploadRecordPopupService: uploadRecordPopupService,
-  //     conversationsService: conversationsService,
-  //     uploadService: uploadService,
-  //     libraryService: libraryService,
-  //     templateService: templateService,
-  //     filesService: filesService,
-  //   };
-  // }
+  describe('sendTemplate', function() {
+    it('should set "vm.videoList"', function() {
+      var scope = $rootScope.$new();
+      var email;
+      var ctrl = $controller('templateCtrl', {
+        $scope: scope,
+        templateService: templateService,
+        libraryService: libraryService,
+        filesService: filesService,
+        conversationsService: conversationsService,
+        profileService: profileService,
+      });
+
+      ctrl.nameObj.name = 'name';
+      ctrl.companyRole.role = 'role';
+      ctrl.sendData = {};
+
+      ctrl.sendTemplate(email);
+      scope.$apply();
+
+      expect(ctrl.showSendPopup).toBe(false);
+      expect($location.path()).toBe('/library');
+    });
+  });
+
+  describe('uploadBtnClick', function() {
+    it('should show upload popup', function() {
+      var scope = $rootScope.$new();
+      var ctrl = $controller('templateCtrl', {
+        $scope: scope,
+        templateService: templateService,
+        libraryService: libraryService,
+        filesService: filesService,
+        conversationsService: conversationsService,
+        profileService: profileService,
+        uploadRecordPopupService: uploadRecordPopupService
+      });
+
+      ctrl.uploadBtnClick();
+
+      expect(uploadRecordPopupService.showUploadPopup).toHaveBeenCalled();
+    });
+  });
+
+  describe('recordBtnClick', function() {
+    it('should show record popup', function() {
+      var scope = $rootScope.$new();
+      var ctrl = $controller('templateCtrl', {
+        $scope: scope,
+        templateService: templateService,
+        libraryService: libraryService,
+        filesService: filesService,
+        conversationsService: conversationsService,
+        profileService: profileService,
+        uploadRecordPopupService: uploadRecordPopupService
+      });
+
+      ctrl.recordBtnClick();
+
+      expect(uploadRecordPopupService.showRecordPopup).toHaveBeenCalled();
+    });
+  });
+
+  describe('edit', function() {
+    it('should set true into "edit mode"', function() {
+      var scope = $rootScope.$new();
+      var ctrl = $controller('templateCtrl', {
+        $scope: scope,
+        templateService: templateService,
+        libraryService: libraryService,
+        filesService: filesService,
+        conversationsService: conversationsService,
+        profileService: profileService,
+      });
+      var element = {};
+
+      ctrl.edit(element);
+      expect(element.editMode).toBe(true);
+    });
+  });
+
+  describe('closeSendPopup', function() {
+    it('should set false into "showSendPopup"', function() {
+      var scope = $rootScope.$new();
+
+      var ctrl = $controller('templateCtrl', {
+        $scope: scope,
+        templateService: templateService,
+        libraryService: libraryService,
+        filesService: filesService,
+        conversationsService: conversationsService,
+        profileService: profileService,
+      });
+
+      ctrl.closeSendPopup();
+
+      expect(ctrl.showSendPopup).toBe(false);
+    });
+  });
+
+  describe('closeSelectFilePopup', function() {
+    it('should set false into "showSelectFilePopup"', function() {
+      var scope = $rootScope.$new();
+
+      var ctrl = $controller('templateCtrl', {
+        $scope: scope,
+        templateService: templateService,
+        libraryService: libraryService,
+        filesService: filesService,
+        conversationsService: conversationsService,
+        profileService: profileService,
+      });
+
+      ctrl.closeSelectFilePopup();
+
+      expect(ctrl.showSelectFilePopup).toBe(false);
+    });
+  });
+
+  describe('closeSelectPopup', function() {
+    it('should set false into "showSelectPopup"', function() {
+      var scope = $rootScope.$new();
+
+      var ctrl = $controller('templateCtrl', {
+        $scope: scope,
+        templateService: templateService,
+        libraryService: libraryService,
+        filesService: filesService,
+        conversationsService: conversationsService,
+        profileService: profileService,
+      });
+
+      ctrl.closeSelectPopup();
+
+      expect(ctrl.showSelectPopup).toBe(false);
+    });
+  });
+
+  describe('deleteFile', function() {
+    it('should delete file from "files"', function() {
+      var scope = $rootScope.$new();
+      var index = 2;
+      var length;
+      var ctrl = $controller('templateCtrl', {
+        $scope: scope,
+        templateService: templateService,
+        libraryService: libraryService,
+        filesService: filesService,
+        conversationsService: conversationsService,
+        profileService: profileService,
+      });
+
+      ctrl.files = [1, 2, 3];
+      length = ctrl.files.length;
+      ctrl.deleteFile(index);
+
+      expect(ctrl.files.length).toBe(length - 1);
+    });
+  });
+
+  describe('onUseFileClick', function() {
+    it('should add file to "files" if file id and id file from files list is equal and files length <= 3', function() {
+      var scope = $rootScope.$new();
+      var showSelectFilePopup;
+      var length;
+      var id = 1;
+      var ctrl = $controller('templateCtrl', {
+        $scope: scope,
+        templateService: templateService,
+        libraryService: libraryService,
+        filesService: filesService,
+        conversationsService: conversationsService,
+        profileService: profileService,
+      });
+
+      ctrl.files = [
+        {
+          id: 1
+        },
+        {
+          id: 2
+        }
+      ];
+      length = ctrl.files.length;
+      ctrl.filesList = [
+        {
+          id: 1
+        }
+      ];
+
+      ctrl.onUseFileClick(id);
+
+      expect(ctrl.files.length).toBe(length + 1);
+    });
+  });
+
+  describe('onUseFileClick', function() {
+    it('should/`nt add file to "files" if file id and id file from files list is/`nt equal and files length <= 3', function() {
+      var scope = $rootScope.$new();
+      var showSelectFilePopup;
+      var length;
+      var id = 3;
+      var ctrl = $controller('templateCtrl', {
+        $scope: scope,
+        templateService: templateService,
+        libraryService: libraryService,
+        filesService: filesService,
+        conversationsService: conversationsService,
+        profileService: profileService,
+      });
+
+      ctrl.files = [
+        {
+          id: 1
+        },
+        {
+          id: 2
+        }
+      ];
+      length = ctrl.files.length;
+      ctrl.filesList = [
+        {
+          id: 1
+        }
+      ];
+
+      ctrl.onUseFileClick(id);
+
+      expect(ctrl.files.length).toBe(length);
+    });
+  });
+
+  describe('onUseFileClick', function() {
+    it('should/`nt add file to "files" if > 3', function() {
+      var scope = $rootScope.$new();
+      var showSelectFilePopup;
+      var length;
+      var id = 1;
+      var ctrl = $controller('templateCtrl', {
+        $scope: scope,
+        templateService: templateService,
+        libraryService: libraryService,
+        filesService: filesService,
+        conversationsService: conversationsService,
+        profileService: profileService,
+      });
+
+      ctrl.files = [
+        {
+          id: 1
+        },
+        {
+          id: 2
+        },
+        {
+          id: 3
+        },
+        {
+          id: 4
+        }
+      ];
+      length = ctrl.files.length;
+      ctrl.filesList = [
+        {
+          id: 1
+        }
+      ];
+
+      ctrl.onUseFileClick(id);
+
+      expect(ctrl.files.length).toBe(length);
+    });
+  });
 });
