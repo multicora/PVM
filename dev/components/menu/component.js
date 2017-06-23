819b81962d6249eef874dd31b48217c3df631826
FeditskiyRoman <feditskiy@gmail.com>
2017-06-23 11:52:09 +0300

    BIZ-379 added menu component

---
+++ dev/components/menu/component.js
@@ -0,0 +1,34 @@
+(function (angular) {
+  'use strict';
+  angular.module('app').component('appMenu', {
+    templateUrl: 'components/menu/tpl.html',
+    controller: ctrl,
+    controllerAs: 'vm',
+    bindings: {
+    }
+  });
+
+  ctrl.$inject = [
+    '$location',
+    'tokenService'
+  ];
+  function ctrl(
+    $location,
+    tokenService
+  ) {
+    var vm = this;
+
+    vm.redirect = function(url, urlParam) {
+      if (urlParam) {
+        return $location.path(url + '/' + urlParam);
+      } else {
+        return $location.path(url);
+      }
+    };
+
+    vm.invalidateSession = function () {
+      tokenService.clearToken();
+      $location.path('/login');
+    };
+  }
+})(angular);
---
+++ dev/components/menu/component.spec.js
@@ -0,0 +1,60 @@
+describe('appMenu component', function() {
+  'use strict';
+
+  var componentController;
+  var bindings;
+  var location;
+  var q;
+  var rootScope;
+  var tokenService;
+
+  beforeEach(module('app'));
+  beforeEach(module(function ($provide) {
+    $provide.value('$location', {
+      path: jasmine.createSpy('path')
+    });
+  }));
+  beforeEach(inject(function($componentController, $location, $q, $rootScope) {
+    componentController = $componentController;
+    location = $location;
+    rootScope = $rootScope;
+    q = $q;
+    bindings = {};
+    tokenService = {
+      clearToken: jasmine.createSpy('clearToken'),
+      getToken: jasmine.createSpy('getToken')
+    };
+  }));
+
+  describe('redirect', function() {
+    it('should redirect to "url/urlParam" if "urlParam" is defined', function() {
+      var url = 'url';
+      var urlParam = 'urlParam';
+      var ctrl = componentController('appMenu', null, bindings);
+
+      ctrl.redirect(url, urlParam);
+      expect(location.path).toHaveBeenCalledWith(url + '/' + urlParam);
+    });
+
+    it('should redirect to "url" if "urlParam" isn/`t defined', function() {
+      var url = 'url';
+      var urlParam;
+      var ctrl = componentController('appMenu', null, bindings);
+
+      ctrl.redirect(url, urlParam);
+      expect(location.path).toHaveBeenCalledWith(url);
+    });
+  });
+
+  describe('invalidateSession', function() {
+    it('should call "tokenService.clearToken" and redirect to "/login"', function() {
+      var ctrl = componentController('appHeader', {
+        tokenService: tokenService
+      }, bindings);
+
+      ctrl.invalidateSession();
+      expect(location.path).toHaveBeenCalledWith('/login');
+      expect(tokenService.clearToken).toHaveBeenCalled();
+    });
+  });
+});
