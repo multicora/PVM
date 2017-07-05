b5d46410a0bb3e84e7d24898002a0f10522199d4
FeditskiyRoman <feditskiy@gmail.com>
2017-07-05 09:52:03 +0300

    BIZ-414 added current page highlight

--- dev/components/menu/component.js
+++ dev/components/menu/component.js
@@ -20,6 +20,18 @@
     var vm = this;
     var tokenName = 'x-biz-token';
 
+    if ($location.url() == '/dashboard') {
+      vm.dashboardClass = 'active-button';
+    } else if ($location.url() == '/conversations') {
+      vm.conversationsClass = 'active-button';
+    } else if ($location.url() == '/library') {
+      vm.libraryClass = 'active-button';
+    } else if ($location.url() == '/contacts') {
+      vm.contactsClass = 'active-button';
+    } else if ($location.url() == '/profile') {
+      vm.profileClass = 'active-button';
+    }
+
     vm.redirect = function(url, urlParam) {
       if (urlParam) {
         return $location.path(url + '/' + urlParam);
