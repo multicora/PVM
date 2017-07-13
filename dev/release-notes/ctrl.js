137eb795e614103ed90efd4bc22e338512fea3e0
FeditskiyRoman <feditskiy@gmail.com>
2017-07-13 17:36:49 +0300

    BIZ-409 added template

--- dev/release-notes/ctrl.js
+++ dev/release-notes/ctrl.js
@@ -9,5 +9,17 @@
   function ctrl(
   ) {
     var vm = this;
+
+    // Should be reworked for geting releases from backend
+    vm.releases = [
+      {
+        date: '1/2/2017',
+        version: '0.0.2a',
+        notes: [
+          'test1',
+          'test2'
+        ]
+      }
+    ]
   }
 })(angular);
