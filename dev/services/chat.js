271ba5839056bb96a98b51e57b83e8410645fcd0
FeditskiyRoman <feditskiy@gmail.com>
2017-08-23 10:32:26 +0300

    BIZ-508 fixed soket.io connection for conversation chat

--- dev/services/chat.js
+++ dev/services/chat.js
@@ -20,33 +20,34 @@
 
     this.connect = function () {
       return $q(function (resolve) {
-        socket.on('connect', function () {
-          authService.getCurrentUser().then(function(res) {
-            currentUser = res.data.id;
-          });
-          socket.on('income', function (data) {
-            pubSub.emit('incomeMessage', data);
-            socket.emit('read', {
-              type: 'read',
-              content: {
-                conversationId: data.conversationId,
-                userId: currentUser
-              }
+        if (!socket.connected) {
+          socket.on('connect', function () {
+            authService.getCurrentUser().then(function(res) {
+              currentUser = res.data.id;
+            });
+            socket.on('income', function (data) {
+              pubSub.emit('incomeMessage', data);
+              socket.emit('read', {
+                type: 'read',
+                content: {
+                  conversationId: data.conversationId,
+                  userId: currentUser
+                }
+              });
             });
           });
+        }
 
-          resolve({
-            send: function (msg, cb) {
-              var data = {
-                content: msg,
-                type: 'chatMessage'
-              };
+        resolve({
+          send: function (msg, cb) {
+            var data = {
+              content: msg,
+              type: 'chatMessage'
+            };
 
-              socket.emit('message', data, cb);
-            }
-          });
+            socket.emit('message', data, cb);
+          }
         });
-
       });
     };
 
