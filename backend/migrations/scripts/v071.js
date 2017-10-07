be059be638902175cf0c600bc95fa8334b7b566d
Dmytro Borchuk <frunk.lern@gmail.com>
2017-10-07 20:35:54 +0300

    Fixed migration

--- backend/migrations/scripts/v071.js
+++ backend/migrations/scripts/v071.js
@@ -6,17 +6,30 @@ const sqlBuilder = require('../../services/sqlBuilder.js');
 module.exports = function() {
   return {
     version: 71,
-    message: 'clear "conversations" table',
+    message: 'clear "files_to_conversations" and "conversations" table',
     script: function (next) {
       const request = sqlBuilder.delete()
-      .from('conversations')
+      .from('files_to_conversations')
       .toString();
 
       connectDB().then( connection => {
         connection.query(request, err => {
-          next(err);
+          if (err) {
+            next(err);
+          } else {
+            const request = sqlBuilder.delete()
+            .from('conversations')
+            .toString();
+
+            connectDB().then( connection => {
+              connection.query(request, err => {
+                next(err);
+              });
+            });
+          }
         });
       });
+
     }
   };
 };
